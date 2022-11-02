b4ca02b7b6eb5efba9f1c0d33175576587594334
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2022-11-02 22:06:43 +0100

    Implement MainApplication.java with @SpringBootApplication annotation

---
+++ src/main/java/de/hbrs/ia/MainApplication.java
@@ -0,0 +1,13 @@
+package de.hbrs.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication  {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
