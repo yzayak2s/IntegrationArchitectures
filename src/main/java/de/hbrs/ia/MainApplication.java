2ba7b920f747e4bbe6d7be1d719abe02daa8fdb0
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2022-11-03 17:40:11 +0100

    MainApplication

--- src/main/java/de/hbrs/ia/MainApplication.java
+++ src/main/java/de/hbrs/ia/MainApplication.java
@@ -1,8 +1,12 @@
 package de.hbrs.ia;
 
+import de.hbrs.ia.repository.SalesManRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories({"de.hbrs.ia.repository"})
 @SpringBootApplication
 public class MainApplication  {
 
