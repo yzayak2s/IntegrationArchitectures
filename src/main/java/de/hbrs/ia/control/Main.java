e776f7bd87eebef5b4ccb3af03a39fec79835f37
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-11-03 18:35:46 +0100

    v1.0.6
    newest version

--- src/main/java/de/hbrs/ia/control/Main.java
+++
@@ -1,9 +0,0 @@
-package de.hbrs.ia.control;
-
-import de.hbrs.ia.model.command.CommandHandler;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        CommandHandler.startInput();
-    }
-}
