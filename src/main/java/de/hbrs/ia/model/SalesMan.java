878b90c8939d3177e18ab12ac4430696f3037617
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2022-11-03 17:46:51 +0100

    SalesMan

--- src/main/java/de/hbrs/ia/model/SalesMan.java
+++ src/main/java/de/hbrs/ia/model/SalesMan.java
@@ -1,16 +1,25 @@
 package de.hbrs.ia.model;
 
-import org.bson.Document;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "SalesMan")
 public class SalesMan {
     private String firstname;
     private String lastname;
+    @Id
     private Integer id;
 
     public SalesMan(String firstname, String lastname, Integer id) {
         this.firstname = firstname;
         this.lastname = lastname;
+
         this.id = id;
+    }
+
+    public SalesMan() {
     }
 
     public String getFirstname() {
@@ -37,11 +46,15 @@ public class SalesMan {
         this.id = id;
     }
 
-    public Document toDocument() {
+    @Override
+    public String toString() {
+        return "SalesMan [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname +"]" ;
+    }
+ /*   public Document toDocument() {
         org.bson.Document document = new Document();
         document.append("firstname" , this.firstname );
         document.append("lastname" , this.lastname );
         document.append("id" , this.id);
         return document;
-    }
+    }*/
 }
