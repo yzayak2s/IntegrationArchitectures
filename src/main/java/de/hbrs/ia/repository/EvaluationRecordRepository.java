c026b79285ccfe97cbe08e61b3bba737858b7df1
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-11-04 12:05:09 +0100

    v1.0.7 (working version)
    added:
     - port 8081 to application.properties
     - springdoc-openapi-ui and spring-boot-starter-data-mongodb dependencies
    
    extensions:
     - EvaluationRecordRepository.java extends MongoRepository<EvaluationRecord,String>
    
    created:
     - OpenApiConfig.java class
    
    implemented:
     - salesmanApi()-method for swagger doc
    
    removed:
     - springfox-boot-starter and springfox-swagger-ui dependencies

--- src/main/java/de/hbrs/ia/repository/EvaluationRecordRepository.java
+++ src/main/java/de/hbrs/ia/repository/EvaluationRecordRepository.java
@@ -1,5 +1,8 @@
 package de.hbrs.ia.repository;
 
+import de.hbrs.ia.model.EvaluationRecord;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 // TODO: 03.11.22 Implementation left
-public interface EvaluationRecordRepository {
+public interface EvaluationRecordRepository extends MongoRepository<EvaluationRecord, String> {
 }
