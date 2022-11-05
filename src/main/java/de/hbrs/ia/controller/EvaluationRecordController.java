f1d5552d6e820f1be66b18aba8257d2771ae7ad3
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-05 17:15:30 +0100

    added openAPI documentation for swagger

--- src/main/java/de/hbrs/ia/controller/EvaluationRecordController.java
+++ src/main/java/de/hbrs/ia/controller/EvaluationRecordController.java
@@ -14,27 +14,51 @@ public class EvaluationRecordController {
     @Autowired
     private EvaluationRecordRepository evaluationRecordRepository;
 
+    /**
+     * Get all Evaluations Records of a Salesman ordered By Year (newest -> oldest)
+     * @param sid Salesman ID
+     * @return List of all Evaluation records
+     */
     @GetMapping("/read/all/{sid}")
     public List<EvaluationRecord> getAllEvaluationRecordsBySalesManID(@PathVariable int sid) {
         return evaluationRecordRepository.findEvaluationRecordsBySalesManIDOrderByYearDesc(sid);
     }
 
+    /**
+     * Get the newest evaluation record af a salesman
+     * @param sid Salesman ID
+     * @return Newest Evaluation Records
+     */
     @GetMapping("/read/{sid}")
     public EvaluationRecord getNewestEvaluationRecordBySalesManID(@PathVariable int sid) {
         return evaluationRecordRepository.findTopEvaluationRecordBySalesManIDOrderByYearDesc(sid);
     }
 
+    /**
+     * Update an evaluation record by its ID
+     * @param goalID Evaluation record ID
+     * @param evaluationRecord the updated evaluation record
+     * @return
+     */
     @PutMapping(value = "/update/{goalID}")
     public EvaluationRecord updateEvaluationRecord(@PathVariable int goalID, @RequestBody EvaluationRecord evaluationRecord) {
         evaluationRecord.setGoalID(goalID);
         return evaluationRecordRepository.save(evaluationRecord);
     }
 
+    /**
+     * Create a new evaluation record
+     * @param evaluationRecord Evaluation Record to be created
+     */
     @PostMapping(value = "/create")
     public void createEvaluationRecord(@RequestBody EvaluationRecord evaluationRecord) {
         evaluationRecordRepository.save(evaluationRecord);
     }
 
+    /**
+     * Delete an evaluation record by its ID
+     * @param goalID Evaluation record ID
+     */
     @DeleteMapping("/delete/{goalID}")
     public void deleteEvaluationRecord(@PathVariable int goalID) {
         evaluationRecordRepository.deleteEvaluationRecordByGoalID(goalID);
--- src/main/java/de/hbrs/ia/controller/SalesManController.java
+++ src/main/java/de/hbrs/ia/controller/SalesManController.java
@@ -14,32 +14,60 @@ public class SalesManController {
     @Autowired
     private SalesManRepository salesManRepository;
 
+    /**
+     * Get a List of All salesman
+     * @return List of all Salesman
+     */
     @GetMapping("/read/all")
     public List<SalesMan> getAllSalesMan() {
         return salesManRepository.findAll();
     }
 
+    /**
+     * Search for all Salesman with the given first name
+     * @param firstname First name of Salesman to search for
+     * @return List of all Salesman with that first name
+     */
     @GetMapping("/read/name/{firstname}")
     public List<SalesMan> getSalesManByFirstname(@PathVariable String firstname) {
         return salesManRepository.findSalesManByFirstname(firstname);
     }
 
+    /**
+     * Search for a salesman with the given ID
+     * @param sid Salesman ID
+     * @return Salesman the ID belongs to
+     */
     @GetMapping("/read/id/{sid}")
     public SalesMan getSalesManById(@PathVariable int sid) {
             return salesManRepository.findSalesManById(sid);
     }
 
+    /**
+     * Create a new salesman
+     * @param salesMan Salesman Object to be created
+     */
     @PostMapping("/create/")
     public void addSalesMan(@RequestBody SalesMan salesMan) {
             salesManRepository.save(salesMan);
     }
 
+    /**
+     * Update a salesman with new data
+     * @param sid Salesman ID
+     * @param salesMan Updates salesman Object
+     * @return The updated salesman object
+     */
     @PutMapping ("/update/{sid}")
     public SalesMan updateSalesMan(@PathVariable int sid, @RequestBody SalesMan salesMan) {
         salesMan.setId(sid);
         return salesManRepository.save(salesMan);
     }
 
+    /**
+     * Delete a salesman by its ID
+     * @param sid Salesman ID
+     */
     @DeleteMapping("/delete/id/{sid}")
     public void deleteSalesMan(@PathVariable int sid) {
         salesManRepository.deleteSalesManById(sid);
